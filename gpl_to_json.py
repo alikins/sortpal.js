6374a2a6bba0e14b9365a0734dc519bf59e2d466
Adrian Likins <adrian@adrian-mac.local>
2010-03-19 15:31:54 -0400

    uniq the colors list when we are generating the json, instead of doing it in jscript at load time. Since that is slow...

--- gpl_to_json.py
+++ gpl_to_json.py
@@ -6,17 +6,20 @@ import sys
 filename = sys.argv[1]
 f = open(filename)
 
-colors = []
 lines = f.readlines()
+# gpl files have a small simple header for the first 3 lines
 lines = lines[3:]
+
+color_dict = {}
 for color in lines:
+	# this is a comment to tell you that # indicates a comment
 	if color[0] == "#":
 		continue
 	bits = color.split()
 	r = int(bits[0])
 	g = int(bits[1])
 	b = int(bits[2])
-	colors.append([r,g,b])
+	# uniq...
+	color_dict[(r,g,b)] = [r,g,b]
 
-print json.dumps(colors)
-
+print json.dumps(color_dict.values())
